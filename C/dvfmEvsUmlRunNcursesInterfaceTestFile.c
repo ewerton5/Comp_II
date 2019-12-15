59e98eebf367abbd13db1cb833929888b6147fd4
Ewerton Vieira <ewerton5@poli.ufrj.br>
2019-12-15 12:02:45 -0300

    Ncurses progredindo

--- C/dvfmEvsUmlRunNcursesInterfaceTestFile.c
+++ C/dvfmEvsUmlRunNcursesInterfaceTestFile.c
@@ -14,26 +14,51 @@ $Log$
 #include					<stdlib.h>
 #include					"dvfmEvsUmlRunNcursesInterface.h"
 
-#define DVFM_EVS_NUMBER_ARGUMENTS			    		2
+#define DVFM_EVS_NUMBER_ARGUMENTS						3
 
-#define DVFM_EVS_OK								    	0
-#define DVFM_EVS_NUMBER_ARGUMENTS_INVALID		        1
+#define DVFM_EVS_OK										0
+#define DVFM_EVS_NUMBER_ARGUMENTS_INVALID				1
+#define DVFM_EVS_FIRST_ARGUMENT_INVALID				    2
+#define DVFM_EVS_FIRST_ARGUMENT_INVALID_HIFEN		    3
+#define DVFM_EVS_SECOND_ARGUMENT_INVALID				4
+#define DVFM_EVS_SECOND_ARGUMENT_INVALID_HIFEN		    5
+
+#define DVFM_EVS_EOS									'\0'
 
 int
 main(int argc,char *argv[ ]) 
 {
-    dvfmEvsUmlErrorType dvfmEvsUmlErrorCode;
+	dvfmEvsUmlErrorType dvfmEvsUmlErrorCode;
     dvfmEvsUmlConfigurationOptionsType *dvfmEvsUmlSettings = (dvfmEvsUmlConfigurationOptionsType *) malloc(sizeof(dvfmEvsUmlConfigurationOptionsType));
+	dvfmEvsUmlLanguageType dvfmEvsUmlLanguage;
+	char *dvfmEvsUmlvalidation;
+
 	if (argc != DVFM_EVS_NUMBER_ARGUMENTS) 
 	{
-		printf("use: %s <nickname>\n", argv[0]);
+		printf("use: %s <nickname> <language_number>\n", argv[0]);
 		exit(DVFM_EVS_NUMBER_ARGUMENTS_INVALID);
 	}
 
-    dvfmEvsUmlErrorCode = DvfmEvsUmlRunNcursesInterface(dvfmEvsUmlSettings, argv[1], dvfmEvsUmlEnglish);
+	if(argv[2][0] == '-')
+	{
+		printf("Error: Second argument invalid\n");
+		printf("First character invalid: \"-\"\n");
+		exit(DVFM_EVS_SECOND_ARGUMENT_INVALID_HIFEN);
+	}
+
+	dvfmEvsUmlLanguage = (dvfmEvsUmlLanguageType) strtoul( argv[2], &dvfmEvsUmlvalidation, 10);
+	if(*dvfmEvsUmlvalidation != DVFM_EVS_EOS)
+	{
+		printf("Error: Argumento dois invalido\n");
+		printf("First character invalid: \"%c\"\n", dvfmEvsUmlvalidation[0]);
+		exit(DVFM_EVS_SECOND_ARGUMENT_INVALID);
+	}
+
+
+    dvfmEvsUmlErrorCode = DvfmEvsUmlRunNcursesInterface(dvfmEvsUmlSettings, argv[1], dvfmEvsUmlLanguage);
 	if (dvfmEvsUmlErrorCode)
     {
-        printf("%s\n", DvfmEvsUmlGetCliErrorMessage(dvfmEvsUmlErrorCode, dvfmEvsUmlEnglish));
+        printf("%s\n", DvfmEvsUmlGetCliErrorMessage(dvfmEvsUmlErrorCode, dvfmEvsUmlLanguage));
         exit(DVFM_EVS_OK);
     }
 
